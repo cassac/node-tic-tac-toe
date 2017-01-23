422753e0e7edb791ac34116a773486e86c080b80
cassac <hicassac@gmail.com>
2017-01-23 14:43:26 -0800

    sudo code for game and board classes

---
+++ game.js
@@ -0,0 +1,21 @@
+// Board class
+  // Constructor
+    // piece on board
+    // array of board
+  // Methods
+    // add piece via coordinates, X or O
+    // Win detection
+      // scan board for win
+        // announce winner if found
+        // end game
+      // if total pieces = 9
+        // end game
+    // Reset board
+      // return board to initial state
+
+// Game
+  // Constructor
+    // turn counter
+  // Methods
+    // Whose turn
+    // Who won
