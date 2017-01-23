61c5f8f77679766060b0820d67a80c212be7e2a4
cassac <hicassac@gmail.com>
2017-01-23 14:57:27 -0800

    expands classes and methods for game and board

--- game.js
+++ game.js
@@ -1,21 +1,50 @@
 // Board class
+class Board {
   // Constructor
-    // piece on board
+  constructor() {
+    // piece count
+    this.count = 0,
     // array of board
+    this.board = Array(3).fill([null, null, null]);
+  }
   // Methods
+  addPiece(x, y, player) {
+    // check if location already occupied
     // add piece via coordinates, X or O
-    // Win detection
-      // scan board for win
-        // announce winner if found
-        // end game
-      // if total pieces = 9
-        // end game
-    // Reset board
-      // return board to initial state
+    this.board[x][y] = player;
+  }
+  // Win detection
+  detectWin() {
+    // scan board for win
+      // announce winner if found
+      // end game
+    // if total pieces = 9
+      // end game
+  }
+  // Reset board
+  initBoard() {
+    // return board to initial state
+    this.board = Array(3).fill([null, null, null]);
+  }
+  printBoard() {
+    // print board to console
+  }
+}
 
 // Game
+class Game {
   // Constructor
-    // turn counter
+  constructor() {
+    // current turn
+    this.turn = Math.round(Math.random());
+  }
   // Methods
-    // Whose turn
-    // Who won
+  // Whose turn
+  nextTurn() {
+    return this.turn ? 'X' : 'O'; 
+  }
+  // Who won
+  currentTurn() {
+    return !this.turn ? 'X' : 'O';
+  }
+}
